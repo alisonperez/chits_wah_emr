fc246a2a64caf89230f35dfdd5034ac9a294be09
alison perez <perez.alison@gmail.com>
2011-04-20 21:01:31 +0800

    new file:   class.html_builder.php

---
+++ chits_query/layout/class.html_builder.php
@@ -0,0 +1,41 @@
+<?php
+// //class would display an html formatted table. entries inside the table can be drilled into patient / household accounts
+class html_builder{
+
+	function html_builder(){
+
+
+	}
+
+	function create_table($width,$header,$cell_contents){
+		echo "<table border='1'>";
+		$this->display_col_header($header);
+
+
+		echo "</table>";
+
+		print_r($width);
+		print_r($header);
+		print_r($cell_contents);
+		print_r($width);
+		print_r($header);
+		print_r($cell_contents);
+		print_r($width);
+		print_r($header);
+		print_r($cell_contents);
+		echo 'alison perez';
+	}
+
+	function display_col_header($header){
+		echo "<tr>";
+		foreach($header as $key=>$header_label){
+			echo "<td>";
+			echo $header_label;
+			echo "</td>";
+		}
+		echo "</tr>";
+
+	}
+}
+
+?>
