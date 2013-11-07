f1380e93dc457dedc4c474b2bdb38027abef77ee
Alison Perez <perez.alison@gmail.com>
2013-11-07 22:38:38 +0800

    modified:   ../chits_query/globals.php

--- chits_query/globals.php
+++ chits_query/globals.php
@@ -1,7 +1,7 @@
 <?
   session_start();
   
-  $dbname = 'ipil_11032013';
+  $dbname = 'chits_live';
   //$dbname = 'camiling_core_data';
   #$dbname2 = 'chitsquery';
   $_SESSION["query"] = $dbname;
